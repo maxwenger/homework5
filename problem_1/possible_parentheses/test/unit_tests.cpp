648fe0d8b05b51df8ba75cfe0cef13cece7e5294
Maxwell Wenger <3391859+maxwenger@users.noreply.github.com>
2019-11-15 21:44:37 -0800

    problem 1 done

--- problem_1/possible_parentheses/test/unit_tests.cpp
+++ problem_1/possible_parentheses/test/unit_tests.cpp
@@ -18,21 +18,32 @@ TEST(test_recursive, 1) {
     std::list<std::string> result;
     possibleParenthesis(1, result);
 	
-    std::list<std::string> answer;
+	ASSERT_EQ(result.back(), "{}");
 }
 
 TEST(test_recursive, 2) {
     std::list<std::string> result;
     possibleParenthesis(2, result);
 
-    // homework: add verification
+	ASSERT_EQ(result.back(), "{{}}");
+	result.pop_back();
+	ASSERT_EQ(result.back(), "{}{}");
 }
 
 TEST(test_recursive, 3) {
     std::list<std::string> result;
     possibleParenthesis(3, result);
 
-    // homework: add verification
+	ASSERT_EQ(result.back(), "{{{}}}");
+	result.pop_back();
+	ASSERT_EQ(result.back(), "{{}{}}");
+	result.pop_back();
+	ASSERT_EQ(result.back(), "{{}}{}");
+	result.pop_back();
+	ASSERT_EQ(result.back(), "{}{{}}");
+	result.pop_back();
+	ASSERT_EQ(result.back(), "{}{}{}");
+
 }
 
 // and more

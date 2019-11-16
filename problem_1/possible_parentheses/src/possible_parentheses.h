d8e5d53caf978da49b293904ffd8767354985347
Maxwell Wenger <3391859+maxwenger@users.noreply.github.com>
2019-11-15 19:18:07 -0800

    init commit

---
+++ problem_1/possible_parentheses/src/possible_parentheses.h
@@ -0,0 +1,9 @@
+#include <list>
+
+// n: size of the problem
+// result: a list of string, each string represents a case of valid parentheses of size n
+void possibleParenthesis(int n, std::list<std::string> & result)
+{
+    // homework
+}
+
